293579fbe4cdb3f1765573a6979ffd7543286d2a
ArturAlmeida2k <arturalmeida2000@hotmail.com>
2026-07-10 17:29:43 +0100

    small fix

--- vh_manual_teleop/vh_network/src/input_teleop_decoder.py
+++ vh_manual_teleop/vh_network/src/input_teleop_decoder.py
@@ -3,7 +3,6 @@ import rclpy
 from rclpy.node import Node
 from rclpy.serialization import deserialize_message
 from rclpy.time import Time
-from rclpy.qos import QoSProfile, ReliabilityPolicy, DurabilityPolicy
 import socket
 from msg_manual_teleop.msg import TeleopCommand
 from msg_manual_teleop.msg import NetworkMetrics
@@ -21,13 +20,7 @@ class InputTeleopDecoder(Node):
 
         self.pub_command = self.create_publisher(TeleopCommand, '/teleop/command', 10)
 
-        metrics_qos = QoSProfile(
-            depth=10,
-            reliability=ReliabilityPolicy.BEST_EFFORT,
-            durability=DurabilityPolicy.VOLATILE
-        )
-
-        self.pub_metrics = self.create_publisher(NetworkMetrics, '/metrics/network/teleop_commands', metrics_qos)
+        self.pub_metrics = self.create_publisher(NetworkMetrics, '/metrics/network/teleop_commands', 10)
         
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(('0.0.0.0', self.port))
