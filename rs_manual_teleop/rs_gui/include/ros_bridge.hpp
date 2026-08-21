47e2d0ca398717241d9e6d2746468739304cfef7
ArturAlmeida2k <arturalmeida2000@hotmail.com>
2026-08-21 11:33:46 +0100

    same as last push

--- rs_manual_teleop/rs_gui/include/ros_bridge.hpp
+++ rs_manual_teleop/rs_gui/include/ros_bridge.hpp
@@ -20,7 +20,7 @@ public:
 
     int64_t nowNanoseconds() { return this->now().nanoseconds(); }
 
-    void publishTelemetryGuiMetrics(uint32_t id, const builtin_interfaces::msg::Time &origin_stamp, double e2e_command, int64_t rx_time_ns, int64_t display_time_ns);
+    double publishTelemetryGuiMetrics(uint32_t id, const builtin_interfaces::msg::Time &origin_stamp, double e2e_command, int64_t rx_time_ns, int64_t display_time_ns);
     void publishFrontCameraMetrics(uint32_t frame_id, double latency_ms);
     void publishFrontCameraNetwork(uint32_t frame_id, double latency_ms);
     void publishFrontCameraDecode(uint32_t frame_id, double latency_ms);
